8e33fdf4a5f09c234c7832be5e41a92a7177b4bd
Dragoslav Pavkovic <dragoslav.pavkovic@gmail.com>
2016-07-22 22:30:46 +0200

    cleanup after complete run

--- src/main/scala/io/vamp/runner/RecipeRunner.scala
+++ src/main/scala/io/vamp/runner/RecipeRunner.scala
@@ -1,76 +1,107 @@
 package io.vamp.runner
 
 import akka.NotUsed
-import akka.actor.ActorLogging
+import akka.actor.{ Actor, ActorLogging }
 import akka.stream._
 import akka.stream.scaladsl.GraphDSL.Implicits._
 import akka.stream.scaladsl.{ Flow, GraphDSL, RunnableGraph, Sink, Source }
+import io.vamp.runner.RunnerActor.{ UpdateDirtyFlag, UpdateState }
 
 import scala.concurrent.Future
 
 trait RecipeRunner extends VampApiClient {
-  this: ActorLogging ⇒
+  this: Actor with ActorLogging ⇒
 
   implicit def materializer: ActorMaterializer
 
   private val recipeActionTimeout = Config.duration("vamp.runner.recipes.timeout")
 
-  protected def update(recipe: Recipe, step: RecipeStep): RecipeStep
-
-  protected def run(recipes: List[Recipe]) = {
-    RunnableGraph.fromGraph(GraphDSL.create() { implicit builder ⇒
-      Source.fromIterator[Recipe](() ⇒ recipes.iterator).flatMapConcat { recipe ⇒
-        Source.fromIterator[RecipeStep](() ⇒ recipe.steps.iterator).map { step ⇒
-          step.copy(state = Recipe.State.Running)
-        }.map(update(recipe, _)).via {
-          runFlow(recipe)
-        }
-      } ~> Sink.last[AnyRef]
-      ClosedShape
+  protected def run(recipes: List[Recipe]): Future[_] = {
+    val result = Sink.last[AnyRef]
+    RunnableGraph.fromGraph(GraphDSL.create(result) { implicit builder ⇒
+      sink ⇒
+        Source.fromIterator[Recipe](() ⇒ recipes.iterator).mapAsync(1) { recipe ⇒
+          Future.sequence {
+            recipe.steps.map { step ⇒
+              run(recipe, step)
+            }
+          } flatMap { _ ⇒
+            Future.sequence {
+              recipe.steps.reverse.map { step ⇒
+                cleanup(recipe, step)
+              }
+            }
+          }
+        } ~> sink.in
+        ClosedShape
     }).run()
   }
 
-  protected def run(recipe: Recipe, step: RecipeStep) = {
-    RunnableGraph.fromGraph(GraphDSL.create() { implicit builder ⇒
-      Source.single(step).map { step ⇒
-        step.copy(state = Recipe.State.Running)
-      }.map(update(recipe, _)) ~>
-        runFlow(recipe) ~>
-        Sink.last[RecipeStep]
-      ClosedShape
+  protected def run(recipe: Recipe, step: RecipeStep): Future[RecipeStep] = {
+    val result = Sink.last[RecipeStep]
+    RunnableGraph.fromGraph(GraphDSL.create(result) { implicit builder ⇒
+      sink ⇒
+        Source.single(step).map { step ⇒
+          self ! UpdateState(recipe, step, Recipe.State.Running)
+          step
+        } ~>
+          runFlow(recipe) ~>
+          sink.in
+        ClosedShape
     }).run()
   }
 
-  protected def cleanup(recipes: List[Recipe]) = {
-    RunnableGraph.fromGraph(GraphDSL.create() { implicit builder ⇒
-      Source.fromIterator[Recipe](() ⇒ recipes.iterator).flatMapConcat { recipe ⇒
-        Source.fromIterator[RecipeStep](() ⇒ recipe.steps.reverseIterator).via {
-          cleanupFlow(recipe)
-        }
-      } ~> Sink.last[AnyRef]
-      ClosedShape
+  protected def cleanup(recipes: List[Recipe]): Future[_] = {
+    val result = Sink.last[AnyRef]
+    RunnableGraph.fromGraph(GraphDSL.create(result) { implicit builder ⇒
+      sink ⇒
+        Source.fromIterator[Recipe](() ⇒ recipes.iterator).mapAsync(1) { recipe ⇒
+          Future.sequence {
+            recipe.steps.reverse.map { step ⇒
+              cleanup(recipe, step)
+            }
+          }
+        } ~> sink.in
+        ClosedShape
+    }).run()
+  }
+
+  protected def cleanup(recipe: Recipe, step: RecipeStep): Future[RecipeStep] = {
+    val result = Sink.last[RecipeStep]
+    RunnableGraph.fromGraph(GraphDSL.create(result) { implicit builder ⇒
+      sink ⇒
+        Source.single(step) ~>
+          cleanupFlow(recipe) ~>
+          sink.in
+        ClosedShape
     }).run()
   }
 
   private def runFlow(recipe: Recipe): Flow[RecipeStep, RecipeStep, NotUsed] = {
-    flow("Run", recipe, { step ⇒ execute(step.cleanup) }, dirty = true)
+    flow("Run", recipe, { step ⇒ execute(step.cleanup) }, { (step, state) ⇒
+      self ! UpdateState(recipe, step, state)
+      self ! UpdateDirtyFlag(recipe, step, dirty = true)
+    })
   }
 
   private def cleanupFlow(recipe: Recipe): Flow[RecipeStep, RecipeStep, NotUsed] = {
-    flow("Cleanup", recipe, { step ⇒ execute(step.cleanup, step.state) }, dirty = false)
+    flow("Cleanup", recipe, { step ⇒ execute(step.cleanup) }, { (step, state) ⇒
+      self ! UpdateDirtyFlag(recipe, step, dirty = false)
+    })
   }
 
-  private def flow(designator: String, recipe: Recipe, action: RecipeStep ⇒ Future[Recipe.State.Value], dirty: Boolean): Flow[RecipeStep, RecipeStep, NotUsed] = {
+  private def flow(designator: String, recipe: Recipe, action: RecipeStep ⇒ Future[Recipe.State.Value], update: (RecipeStep, Recipe.State.Value) ⇒ Unit): Flow[RecipeStep, RecipeStep, NotUsed] = {
     Flow[RecipeStep].mapAsync(1) { step ⇒
       action(step).map {
         state ⇒
           log.info(s"$designator - ${state.toString.toLowerCase}: [${recipe.name} :: ${step.description}]")
-          update(recipe, step.copy(state = state, dirty = dirty))
+          update(step, state)
+          step
       }
     }.completionTimeout(recipeActionTimeout)
   }
 
-  private def execute(action: RecipeStepAction, success: Recipe.State.Value = Recipe.State.Succeeded): Future[Recipe.State.Value] = {
+  private def execute(action: RecipeStepAction): Future[Recipe.State.Value] = {
 
     def recover: AnyRef ⇒ Recipe.State.Value = {
       failure ⇒
@@ -80,7 +111,7 @@ trait RecipeRunner extends VampApiClient {
 
     def result: AnyRef ⇒ Recipe.State.Value = {
       case Right(_) ⇒ Recipe.State.Failed
-      case _        ⇒ success
+      case _        ⇒ Recipe.State.Succeeded
     }
 
     action.method match {
--- src/main/scala/io/vamp/runner/RunnerActor.scala
+++ src/main/scala/io/vamp/runner/RunnerActor.scala
@@ -1,6 +1,7 @@
 package io.vamp.runner
 
 import akka.actor.{ Actor, ActorLogging, ActorSystem, Props }
+import akka.agent.Agent
 import akka.stream.ActorMaterializer
 import io.vamp.runner.Hub.Broadcast
 
@@ -17,6 +18,10 @@ object RunnerActor {
   case class Cleanup(arguments: AnyRef)
 
   case class Recipes(recipes: List[Recipe]) extends Response
+
+  case class UpdateState(recipe: Recipe, step: RecipeStep, state: Recipe.State.Value)
+
+  case class UpdateDirtyFlag(recipe: Recipe, step: RecipeStep, dirty: Boolean)
 }
 
 class RunnerActor(implicit val materializer: ActorMaterializer) extends Actor with ActorLogging with RecipeLoader with RecipeRunner {
@@ -29,38 +34,55 @@ class RunnerActor(implicit val materializer: ActorMaterializer) extends Actor wi
 
   private val recipes = mutable.LinkedHashMap(load.map(recipe ⇒ recipe.id -> recipe): _*)
 
-  def receive: Receive = {
-    case ProvideRecipes     ⇒ sender() ! Recipes(recipes.values.toList)
-    case Run(arguments)     ⇒ run()(arguments)
-    case Cleanup(arguments) ⇒ cleanup()(arguments)
-    case _                  ⇒
-  }
+  private val running = Agent[Boolean](false)
 
-  override protected def update(recipe: Recipe, step: RecipeStep): RecipeStep = {
-    recipes.get(recipe.id).map { r ⇒
-      recipes += (r.id -> r.copy(steps = r.steps.map { s ⇒
-        if (s.id == step.id) step else s
-      }))
-    }
-    context.parent ! Broadcast(Recipes(RunnerActor.this.recipes.values.toList))
-    step
+  def receive: Receive = {
+    case ProvideRecipes                       ⇒ sender() ! Recipes(recipes.values.toList)
+    case Run(arguments)                       ⇒ if (!running.get()) run()(arguments)
+    case Cleanup(arguments)                   ⇒ if (!running.get()) cleanup()(arguments)
+    case UpdateState(recipe, step, state)     ⇒ update(recipe, step, state)
+    case UpdateDirtyFlag(recipe, step, dirty) ⇒ update(recipe, step, dirty)
+    case _                                    ⇒
   }
 
   private def run(): PartialFunction[AnyRef, Unit] = {
     case list: List[_] ⇒
+      running.send(true)
       run(ids2recipes(list).map { recipe ⇒
         recipes += (recipe.id -> recipe.copy(steps = recipe.steps.map { step ⇒ step.copy(state = Recipe.State.Idle) }))
         recipe
-      })
+      }).onComplete(_ ⇒ running.send(false))
 
     case map: Map[_, _] ⇒ for {
       recipe ← map.asInstanceOf[Map[String, _]].get("recipe").flatMap(id ⇒ recipes.get(id.toString))
       step ← map.asInstanceOf[Map[String, _]].get("step").flatMap(id ⇒ recipe.steps.find(_.id == id))
-    } yield run(recipe, step)
+    } yield {
+      running.send(true)
+      run(recipe, step).onComplete(_ ⇒ running.send(false))
+    }
   }
 
   private def cleanup(): PartialFunction[AnyRef, Unit] = {
-    case list: List[_] ⇒ cleanup(ids2recipes(list))
+    case list: List[_] ⇒
+      running.send(true)
+      cleanup(ids2recipes(list)).onComplete(_ ⇒ running.send(false))
+  }
+
+  private def update(recipe: Recipe, step: RecipeStep, state: Recipe.State.Value): Unit = {
+    update(recipe, step, { step ⇒ step.copy(state = state) })
+  }
+
+  private def update(recipe: Recipe, step: RecipeStep, dirty: Boolean): Unit = {
+    update(recipe, step, { step ⇒ step.copy(dirty = dirty) })
+  }
+
+  private def update(recipe: Recipe, step: RecipeStep, update: RecipeStep ⇒ RecipeStep): Unit = {
+    recipes.get(recipe.id).map { r ⇒
+      recipes += (r.id -> r.copy(steps = r.steps.map { s ⇒
+        if (s.id == step.id) update(s) else s
+      }))
+    }
+    context.parent ! Broadcast(Recipes(RunnerActor.this.recipes.values.toList))
   }
 
   private def ids2recipes(ids: List[_]): List[Recipe] = ids.flatMap(id ⇒ recipes.get(id.toString))
