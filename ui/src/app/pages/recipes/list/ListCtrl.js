9c8347f640a1aff2187bff6287f1e1aba7d9f37c
Bastiaan Niamut <bastiaan@magnetic.io>
2016-09-28 16:41:06 +0200

    Fixes #56, Fixes #60

--- ui/src/app/pages/recipes/list/ListCtrl.js
+++ ui/src/app/pages/recipes/list/ListCtrl.js
@@ -7,11 +7,20 @@
   /** @ngInject */
   function ListCtrl($rootScope, $scope, baConfig, api) {
 
+
     $scope.transparent = baConfig.theme.blur;
 
     $scope.recipes = api.recipes;
 
     $scope.details = function (recipe) {
+      $scope.recipes.forEach(function(theRecipe) {
+        theRecipe.active = false;
+      })
+
+      recipe.active = true;
+
+      console.log(recipe.active);
+
       $rootScope.$emit('recipe:details', recipe);
     };
   }
