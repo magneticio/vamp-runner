3b3f1ddb1241e8af30285a09143171c64503bcc5
Dragoslav Pavkovic <dragoslav.pavkovic@gmail.com>
2017-02-11 10:31:15 +0100

    adding recipe steps

--- ng2/src/app/app.module.ts
+++ ng2/src/app/app.module.ts
@@ -1,24 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-import { MaterialModule } from '@angular/material';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {MaterialModule} from '@angular/material';
 
 import 'hammerjs';
-import { AppComponent, DialogContent } from './app.component';
+import {AppComponent} from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent, DialogContent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    HttpModule,
     MaterialModule.forRoot()
   ],
   providers: [],
-  entryComponents: [DialogContent],
+  entryComponents: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
