862829559ac5046fe5ebd723c998a7c29578d25f
Dragoslav Pavkovic <dragoslav.pavkovic@gmail.com>
2017-02-15 20:22:45 +0100

    width calculation

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -29,7 +29,7 @@ export class AppComponent {
   }
 
   wide() {
-    return window.screen.width >= 768;
+    return Math.max(document.documentElement.clientWidth, window.innerWidth || 0) >= 768;
   }
 
   updateRecipes(recipes: Recipe[]) {
